25bc1cf23f7aed681a5187f96f3d6491afaa1f8e
Gabriel Anguiano <anguianog97@gmail.com>
2019-10-03 14:47:07 -0700

    finished mvp working on stretch goals

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,10 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/Gabe0214')
+.then(response => {
+  console.log(response); 
+})
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,10 +28,26 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+  const followersArray = ['ddelfaus', 'SteveMM-III', 'mranthonysutton', 'Sara-DLC', 'primelos'];
+  
+  followersArray.forEach(person => {
+    axios.get(`https://api.github.com/users/${person}` )
+    .then( (response => {
+      const person = response
+      cardContainer.appendChild(gitCard(person));
+    }))
+   .catch(error => {
+     console.log('There is an error in the data', error);
+   })
+   })
+
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
+
+  
+
 
 <div class="card">
   <img src={image url of user} />
@@ -53,3 +73,75 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+
+function gitCard(object){
+  // elements created 
+
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const cardHeader = document.createElement('h3');
+  const userName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const anchor = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+
+  // classe assign
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  cardHeader.classList.add('name');
+  userName.classList.add('username');
+
+
+  // textContent
+  
+  image.setAttribute('src', object.data["avatar_url"]);
+  cardHeader.textContent = object.data.name;
+  userName.textContent = object.data.login;
+  location.textContent = ` Location: ${object.data.location}`;
+  profile.textContent = 'Profile: '
+  anchor.setAttribute('href',object.data["html_url"]);
+  anchor.textContent = ' CLICK HERE!! ';
+  followers.textContent =  ` Followers: ${object.data.followers}`;
+  following.textContent = `Following: ${object.data.following}`; 
+  bio.textContent = ` Bio: ${object.data.bio}`; 
+
+
+ //style :
+anchor.style.textDecoration ='none';
+anchor.style.color = '#333'; 
+
+
+  // append 
+
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(cardHeader)
+  cardInfo.appendChild(userName)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile);
+  profile.appendChild(anchor);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  return card;
+}
+
+const cardContainer = document.querySelector('.cards');
+
+axios.get('https://api.github.com/users/Gabe0214')
+.then(response => {
+  cardContainer.appendChild(gitCard(response));
+})
+.catch(error => {
+  cosnsole.log('The data was not returned', error);
+})
+
+
